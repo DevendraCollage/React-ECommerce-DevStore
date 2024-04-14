feb3eb352efbd441e8d4b9e0ad71a83bada35357
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-04-14 17:05:27 +0530

    Fetch the API using the Axios and use the useReducer

---
+++ src/Reducer/productsReducer.js
@@ -0,0 +1,51 @@
+const productReducer = (state, action) => {
+  //? Or also you can done like this
+  //   if (action.type === "SET_LOADING") {
+  //     // When Data is Not Load
+  //     return {
+  //       ...state,
+  //       isLoading: true,
+  //     };
+  //   }
+  //   if (action.type === "MY_API_ERROR") {
+  //     // Data not displayed
+  //     return {
+  //       ...state,
+  //       isLoading: false,
+  //       isError: true,
+  //     };
+  //   }
+
+  //? You can also done like this
+  switch (action.type) {
+    case "SET_LOADING":
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case "SET_API_DATA":
+      const featureData = action.payload.filter((curElem) => {
+        return curElem.featured === true;
+      });
+
+      return {
+        ...state,
+        isLoading: false,
+        products: action.payload,
+        featureProducts: featureData,
+      };
+
+    case "API_ERROR":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default productReducer;
--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -2,18 +2,48 @@
 //* 2. Provider
 //* 3. consumer => useContext Hook
 
-import { useContext, createContext } from "react";
+import { useContext, createContext, useEffect, useReducer } from "react";
+import axios from "axios";
+import reducer from "../Reducer/productsReducer";
 
 //? 1. First Step Create the Context
 const AppContext = createContext();
 
+//? Create the API Variable
+const API = "https://661bb1ed65444945d05012a9.mockapi.io/DevStore";
+
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featuredProducts: [], // We are using featured products as in the API if we use featured True then an then we will fetch those products
+};
+
 //? 2. Second Step create the Provider
 //* This children is the now work as the App Component
 const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  //? Define the getProducts method and fetch the data using axios third party library
+  const getProduct = async (url) => {
+    dispatch({ type: "SET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      // console.log(res); for check the res in the console
+      const products = await res.data;
+      dispatch({ type: "SET_API_DATA", payload: products });
+    } catch (error) {
+      dispatch({ type: "MY_API_ERROR" });
+    }
+  };
+
+  //? Call the API Once that for use the useEffect
+  useEffect(() => {
+    getProduct(API);
+  }, []);
+
   return (
-    <AppContext.Provider value={{ myName: "Devendra Parmar" }}>
-      {children}
-    </AppContext.Provider>
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
   );
 };
 
